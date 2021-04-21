ea2d42cb444580f1f87c2f3058ae7b3063dbf4f2
kholostyak17 <kholostyak17@gmail.com>
2021-04-21 13:04:30 +0000

    improved .concat() and saved all domains in an array

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let pronoun = ['the', 'our', 'under',''];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let doms = ['.com','.es','.pl','.org','.net'];
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const doms = ['.com','.org','.net'];
+let arrayDominios = [];
+
 for (let index1 = 0; index1 < pronoun.length; index1++) {
     for (let index2 = 0; index2 < adj.length; index2++) {
         for (let index3 = 0; index3 < noun.length; index3++) {
             for (let index4 = 0; index4 < doms.length; index4++) {
-               console.log(pronoun[index1].concat(adj[index2].concat(noun[index3].concat(doms[index4]))));           
+               let dominioCompleto = "";
+                console.log(dominioCompleto.concat(pronoun[index1], adj[index2],noun[index3], doms[index4]));           
+                arrayDominios.push(dominioCompleto.concat(pronoun[index1], adj[index2],noun[index3], doms[index4]));
             }           
         }
     }
 }
+console.log(arrayDominios);
 
