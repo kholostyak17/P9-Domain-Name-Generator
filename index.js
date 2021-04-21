a072f80b6241766469bbed52ebf759239618f29d
kholostyak17 <kholostyak17@gmail.com>
2021-04-21 13:36:50 +0000

    implemented - print domains in the index.html

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const pronoun = ['the', 'our'];
 const adj = ['great', 'big'];
 const noun = ['jogger', 'racoon'];
-const doms = ['.com','.org','.net'];
+const doms = ['.ces', '.org', '.net'];
 let arrayDominios = [];
 
-for (let index1 = 0; index1 < pronoun.length; index1++) {
-    for (let index2 = 0; index2 < adj.length; index2++) {
-        for (let index3 = 0; index3 < noun.length; index3++) {
-            for (let index4 = 0; index4 < doms.length; index4++) {
-               let dominioCompleto = "";
-                console.log(dominioCompleto.concat(pronoun[index1], adj[index2],noun[index3], doms[index4]));           
-                arrayDominios.push(dominioCompleto.concat(pronoun[index1], adj[index2],noun[index3], doms[index4]));
-            }           
+window.onload = () => {
+    for (let index1 = 0; index1 < pronoun.length; index1++) {
+        for (let index2 = 0; index2 < adj.length; index2++) {
+            for (let index3 = 0; index3 < noun.length; index3++) {
+                for (let index4 = 0; index4 < doms.length; index4++) {
+                    let dominioCompleto = "";
+                    console.log(dominioCompleto.concat(pronoun[index1], adj[index2], noun[index3], doms[index4]));
+                    arrayDominios.push(dominioCompleto.concat(pronoun[index1], adj[index2], noun[index3], doms[index4]));
+                }
+            }
         }
     }
-}
-console.log(arrayDominios);
+    console.log('este es el array completo', arrayDominios);
+    for (let elementos = 0; elementos < arrayDominios.length; elementos++) {
+        let stringparrafo = ""
+        document.querySelector("#body").innerHTML += stringparrafo.concat('<p id="', elementos, '">', arrayDominios[elementos], '</p>');
+    }
 
+} 
