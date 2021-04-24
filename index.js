f0146891ef3fa090d7823c4587f40588b46a7c17
kholostyak17 <kholostyak17@gmail.com>
2021-04-24 21:15:20 +0000

    changed all words

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const verbo = ['compra', 'regala', 'comparte'];
-const pronombre = ['tus', 'mis'];
-const sustantivo = ['cancion', 'pec', 'video'];
-const dominios = ['.es', '.os'];
-let arrayDominios = [];
+const word1 = ['aprende', 'comparte'];
+const word2 = ['con', ''];
+const word3 = ['profesor', 'dulc', 'pec', 'inter', 'calid'];
+const domains = ['.es', '.net', '.ad'];
+let arrayWebsite = [];
 
 window.onload = () => {
-    for (let index1 = 0; index1 < verbo.length; index1++) {
-        for (let index2 = 0; index2 < pronombre.length; index2++) {
-            for (let index3 = 0; index3 < sustantivo.length; index3++) {
-                for (let index4 = 0; index4 < dominios.length; index4++) {
+    for (let index1 = 0; index1 < word1.length; index1++) {
+        for (let index2 = 0; index2 < word2.length; index2++) {
+            for (let index3 = 0; index3 < word3.length; index3++) {
+                for (let index4 = 0; index4 < domains.length; index4++) {
                     let dominioCompleto = "";
-                    console.log(dominioCompleto.concat(verbo[index1], pronombre[index2], sustantivo[index3], dominios[index4]));
-                    arrayDominios.push(dominioCompleto.concat(verbo[index1], pronombre[index2], sustantivo[index3], dominios[index4]));
+                    let finalword = word3[index3].concat(domains[index4]);
+                    let verb = word1[index1].concat(word2[index2]);
+                    if (finalword == 'profesor.es' || finalword == 'dulc.es' || finalword == 'pec.es' || finalword == 'inter.net' || finalword == 'calid.ad') {
+                        if (verb == 'aprendecon'||verb =='comparte')
+                        console.log(dominioCompleto.concat(word1[index1], word2[index2], word3[index3], domains[index4]));
+                        arrayWebsite.push(dominioCompleto.concat(word1[index1], word2[index2], word3[index3], domains[index4]));
+                    }
+
                 }
             }
         }
     }
-    console.log('este es el array completo', arrayDominios);
-    for (let elementos = 0; elementos < arrayDominios.length; elementos++) {
+    console.log('este es el array completo', arrayWebsite);
+    for (let elementos = 0; elementos < arrayWebsite.length; elementos++) {
         let stringparrafo = ""
-        document.querySelector("#body").innerHTML += stringparrafo.concat('<p id="', elementos, '">', arrayDominios[elementos], '</p>');
+        document.querySelector("#body").innerHTML += stringparrafo.concat('<p id="', elementos, '">', arrayWebsite[elementos], '</p>');
     }
 
 } 
