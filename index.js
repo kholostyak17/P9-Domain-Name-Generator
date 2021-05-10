02996ba75dbfc065f19d20754367149acd0a87bf
kholostyak17 <kholostyak17@gmail.com>
2021-05-10 06:22:28 +0000

    fixed descriptive nouns

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const word1 = ['aprende', 'comparte', 'piensa', 'habla'];
-const word2 = ['con', '', 'en', 'sobre'];
-const word3 = ['profesor', 'dulc', 'pec', 'inter', 'calid'];
+const verb = ['aprende', 'comparte', 'piensa', 'habla'];
+const preposition = ['con', '', 'en', 'sobre'];
+const substantive = ['profesor', 'dulc', 'pec', 'inter', 'calid'];
 const domains = ['.es', '.net', '.ad'];
 
 
 window.onload = () => {
     let arrayWebsite = [];
     let listElement;
-    for (let index1 = 0; index1 < word1.length; index1++) {
-        for (let index2 = 0; index2 < word2.length; index2++) {
-            for (let index3 = 0; index3 < word3.length; index3++) {
+    for (let index1 = 0; index1 < verb.length; index1++) {
+        for (let index2 = 0; index2 < preposition.length; index2++) {
+            for (let index3 = 0; index3 < substantive.length; index3++) {
                 for (let index4 = 0; index4 < domains.length; index4++) {
                     let website = "";
-                    let word3domain = word3[index3].concat(domains[index4]);
-                    let word12 = word1[index1].concat(word2[index2]);
-                    if (word3domain == 'profesor.es' || word3domain == 'dulc.es' || word3domain == 'pec.es' || word3domain == 'inter.net' || word3domain == 'calid.ad') {
-                        if (word12 == 'aprendecon' || word12 == 'comparte' || word12 == 'piensaen' || word12 == 'hablasobre') {
-                            console.log(website.concat(word1[index1], word2[index2], word3[index3], domains[index4]));
-                            arrayWebsite.push(website.concat(word1[index1], word2[index2], word3[index3], domains[index4]));
+                    let substDomain = substantive[index3].concat(domains[index4]);
+                    let verbPrep = verb[index1].concat(preposition[index2]);
+                    if (substDomain == 'profesor.es' || substDomain == 'dulc.es' || substDomain == 'pec.es' || substDomain == 'inter.net' || substDomain == 'calid.ad') {
+                        if (verbPrep == 'aprendecon' || verbPrep == 'comparte' || verbPrep == 'piensaen' || verbPrep == 'hablasobre') {
+                            console.log(website.concat(verb[index1], preposition[index2], substantive[index3], domains[index4]));
+                            arrayWebsite.push(website.concat(verb[index1], preposition[index2], substantive[index3], domains[index4]));
                         }
 
                     }
